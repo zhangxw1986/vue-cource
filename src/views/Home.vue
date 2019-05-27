ed72d491021445db61c676aab33db22f749ffca5
jin <jin@jin.local>
2019-05-27 22:34:05 +0800

    mac push

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,11 +25,13 @@ export default {
       // this.$router.go(1)等同于history.forward()
       if (type === "back") this.$router.back();
       // else if (type === "push") this.$router.push("parent");
+      // 路由传递参数4种方式：1、通过router-link的to参数传参 2、在路由配置中利用动态路由配置 3、编程式导航中利用params对象或query对象传参
       else if (type === "push") this.$router.push({
         name:'argu',
         params: {
           name: 'zxw'
         },
+        //
         query: {
           name: 'zxw'
         }
